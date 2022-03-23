38969b41583eac843bddfaeeb0307f6a7ef6c8b4
Soebb <87156166+Soebb@users.noreply.github.com>
2022-03-23 05:17:28 +0430

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# system path to chromedriver.exe
+CHROMEDRIVER_PATH = r"C:\chromedriver.exe"
+
+
+def fix_encoding(text):
+    
+
+text = input("Paste text: ")
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+browser = webdriver.Chrome(service=Service(CHROMEDRIVER_PATH),options=options)
+
+link_to_online_corrector = "https://dadmatech.ir/#/products/SpellChecker"
+browser.get(link_to_online_corrector)
+
+text_field = browser.find_element(By.XPATH, '/html/body/div/main/div/div[3]/div[2]/form/div/div/div[1]/div')
+
+# replace the default(testing) text in the field with our text
+text_field.send_keys(Keys.CONTROL+'a')
+text_field.send_keys(text)
+
+submit_button = browser.find_element(By.XPATH, '/html/body/div/main/div/div[3]/div[2]/form/div/div/div[1]/button')
+submit_button.click()
+
+correct_field = WebDriverWait(browser, 20).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div/main/div/div[3]/div[2]/form/div/div/div[3]/div/div[1]')))
+
+corrected_text = fix_encoding(correct_field.text)
+
+print(corrected_text)
